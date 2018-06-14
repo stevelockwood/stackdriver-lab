64e80fc2750fe51c2e7ed2287204d554d04e829a
Jeff Davis <jeff@highfive.com>
2018-06-14 16:45:51 -0700

    make scripts executable

---
+++ activity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# get projectid
+# get sql instance ip
+# get load balancer ip
+
+# generate apache 200s
+# generate apache 404s
+# generate apache 403s
+
+# generate nginx 200s
+# generate nginx 404s
+# generate nginx 403s
+
+# generate sql permission denied
+# generate sql bad db
+
+# generate pubsub activity
