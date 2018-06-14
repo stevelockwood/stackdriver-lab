e4550d4cbc4b2668b60f597ec8df272963b0ce90
Jeff Davis <jeff@highfive.com>
2018-06-14 11:04:42 -0700

    add firewall rule for linux server

--- setup.sh
+++ setup.sh
@@ -8,3 +8,5 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAcco
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:windows-servers@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role='roles/monitoring.metricWriter'
 gcloud compute instances create linux-server --service-account linux-servers@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --zone us-central1-a --metadata-from-file startup-script=linux_startup.sh
 gcloud compute instances create windows-server --service-account windows-servers@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --image-project windows-cloud --image windows-server-1803-dc-core-v20180508 --zone us-central1-a --metadata-from-file windows-startup-script-ps1=windows_startup.ps1
+gcloud compute instances add-tags linux-server --zone us-central1-a --tags http-server
+gcloud compute firewall-rules create http-server --allow tcp:80 --target-tags http-server
