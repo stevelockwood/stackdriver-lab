ded519349898e75395f217252989683546efe7fd
jwdavis <jeff@jwdavis.me>
2018-06-14 21:36:16 -0700

    apache config

--- linux_startup.sh
+++ linux_startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
   if [ ! -f /initialized.txt ]; then
-    # install apache
+    # install apache, git
     apt-get update
-    apt-get install -y apache2
+    apt-get install -y apache2, git
 
     # install logging agent
     curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
@@ -11,6 +11,15 @@
     # install monitoring agent
     curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
     bash install-monitoring-agent.sh
+
+    # clone repo
+    cd /
+    git clone https://github.com/jwdavis/stackdriver-lab.git
+    cd /stackdriver-lab
+
+    # update apache config
+    cp apache2.conf /etc/apache2/apache2.conf
+    /etc/init.d/apache2 reload
 
     # create file showing setup
     touch /initialized.txt
